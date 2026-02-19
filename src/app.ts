1a249f5e55cff0a8d93a06139dd31d571cfc5433
UP220628 <up220628@alumnos.upa.edu.mx>
2026-02-19 08:57:20 -0600

    x-user-rol

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const corsOptions = {
   origin: env.corsOrigin.split(',').map(o => o.trim()),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-role', 'x-user-id'],
   maxAge: 86400,
 };
 
